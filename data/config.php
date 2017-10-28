86283371239651c7ca4d9bfa4eb92f7fb41e7834
Marvin <GamerFac3@gmail.com>
2017-10-28 17:32:35 +0200

    Code Cleanup and added reCAPTCHA

--- data/config.php
+++ data/config.php
@@ -1,10 +1,11 @@
 <?php
 
-$HOST_QUERY = "xxxxxx"; 
+$HOST_QUERY = "xxxxx";
 $PORT_QUERY = "10011"; 
 $USER_QUERY = "serveradmin"; 
-$PASS_QUERY = "xxxxx"; 
+$PASS_QUERY = "xxxxx";
 $NICK_QUERY = "xxxxx";
-
+$GOOGLE_CAPTCHA_PUBLICKEY = "xxxxx";
+$GOOGLE_CAPTCHA_PRIVATEKEY = "xxxxx";
 
 ?>
